a48316fa6fe2648f6847ad8c0bcfc490be6bb2b8
fantomx11 <fantomx11@gmail.com>
2026-02-16 16:46:36 -0600

    Add files via upload

---
+++ Event.ts
@@ -0,0 +1,8 @@
+export enum MathZeldaEvent {
+
+  ACTOR_HURT = "actor_hurt",
+  ACTOR_HP_CHANGE = "ACTOR_HP_CHANGE"
+
+
+
+}
---
+++ PickupModel.ts
@@ -0,0 +1,48 @@
+import { PlayerModel } from './PlayerModel.js';
+import { EntityConfig, EntityModel, ISceneWithItemDrops } from './EntityModel.js';
+
+ /** Represents an item on the ground that can be picked up.
+ */
+export class PickupModel extends EntityModel {
+  public subtype: string;
+  pickedUp: boolean = false;
+
+  constructor(x: number, y: number, subtype: string, scene: ISceneWithItemDrops, config?: EntityConfig) {
+    super(x, y, scene, {type: 'pickup', ...config});
+    this.subtype = subtype;
+  }
+
+  /**
+   * Pickups don't have AI.
+   */
+  tick(): boolean {
+    return this.pickedUp;
+  }
+
+  /**
+   * Pickups cannot take damage from attacks.
+   */
+  takeDamage(amount: number, srcX: number, srcY: number): boolean {
+    return false;
+  }
+
+  getAnimKey(): string {
+    return `item_${this.subtype}`;
+  }
+
+  /**
+   * Called when a player collides with this pickup.
+   * The base implementation does nothing and can be overridden by subclasses.
+   * @param player The player model that collected this pickup.
+   * @returns True if the pickup was successfully collected, false otherwise.
+   */
+  onPickup(player: PlayerModel): boolean {
+    return true;
+  }
+
+  onTouch(other: EntityModel): void {
+    if (other.type === 'player' && this.onPickup(other as PlayerModel)) {
+      this.pickedUp = true;
+    };
+  }
+}
